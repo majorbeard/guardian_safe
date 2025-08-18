2f5f421041d320b40b74c3f56ed666ba3ae4e752
majorbeard <duplessis.werner@hotmail.com>
2025-08-18 19:59:28 +0200

    Initial commit

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Guardian Safe</h1>
+
+      <div>
+        <p class="text-lg">Your one-stop solution for online safety.</p>
+      </div>
+    </>
+  );
+}
